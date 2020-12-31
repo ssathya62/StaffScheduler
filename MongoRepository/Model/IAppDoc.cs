9b89d46c90c666eeecbeb6e65a3bf4b4e88a08bc
Sridhar Sathya <sridhar_0314@live.com>
2020-12-31 11:10:58 -0500

    fixed AZ#621

---
+++ MongoRepository/Model/IAppDoc.cs
@@ -0,0 +1,10 @@
+﻿using MongoDbGenericRepository.Models;
+using System;
+
+namespace MongoRepository.Model
+{
+	public interface IAppDoc : IDocument<string>
+	{		
+		DateTime ComputeDate { get; set; }
+	}
+}
