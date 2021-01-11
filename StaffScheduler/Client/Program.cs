6c14be69ae59a944d637ebd93a715ecac7a210c0
Sridhar Sathya <sridhar_0314@live.com>
2021-01-11 11:11:05 -0500

    Error in main layout.
    Pages were not rendered. Fixed before proceeding.

--- StaffScheduler/Client/Program.cs
+++ StaffScheduler/Client/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace StaffScheduler.Client
@@ -14,11 +11,18 @@ namespace StaffScheduler.Client
 	{
 		public static async Task Main(string[] args)
 		{
+
+			
+
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			var keyPart = builder.Configuration["Syncfusion:Erandhu"];
+			var keyaPart = builder.Configuration["Syncfusion:Mudhal"];
 
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(keyaPart.Trim()+keyPart.Trim());			
+			builder.Services.AddSyncfusionBlazor();
 			await builder.Build().RunAsync();
 		}
 	}
